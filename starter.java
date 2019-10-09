5be81beab20d7ddb90a41d42d7a7e74de9747ca0
andrew boyle <aboyle3100@gmail.com>
2019-10-09 11:39:08 -0700

    Initial volleyball stuff

---
+++ starter.java
@@ -0,0 +1,12 @@
+public class starter 
+{
+		
+        public static void main(String args[])
+        {
+			SimplePicture X = new Picture("blackX.png");
+			X.drawString("joe", 50,50);
+			X.show();
+		}
+		
+	
+}
